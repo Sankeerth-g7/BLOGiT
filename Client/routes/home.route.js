c2803e7b3c197da6482dc85d0985e85479fc3d41
Sankeerth-g7 <20951A04G7@iare.ac.in>
2022-11-24 02:14:27 +0530

    port config env var

--- Client/routes/home.route.js
+++ Client/routes/home.route.js
@@ -26,8 +26,8 @@ router.get('/blogs', (req, res) => {
             }
             articles.sort((o) => { return o.date })
             articles.reverse()
-            res.send(articles)
-            // res.render('home/blogs', {articles: articles})
+            // res.send(articles)
+            res.render('home/blogs', {articles: articles})
         }
         else{
             res.status(404).render(404)
